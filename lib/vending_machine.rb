5d36287416883946abd0c64d16d5d6eaab486a73
Kenichiroh Ota <oota_ken@hotmail.com>
2012-05-26 20:39:52 +0900

    [from now] 2012/05/26 20:39:52
    diff --git a/lib/vending_machine.rb b/lib/vending_machine.rb
    index 60cb6ab..6157567 100644
    --- a/lib/vending_machine.rb
    +++ b/lib/vending_machine.rb
    @@ -8,8 +8,13 @@ class VendingMachine
         @amount
       end
    
    -  def drop_in(money)
    -    @amount = money
    +  def drop_in(*money)
    +    @amount = money.inject(0) do |sum, item|
    +      if item == 1 || item == 5
    +        raise "invalid money"
    +      end
    +      sum += item
    +    end
       end
     end
    
    diff --git a/spec/vending_machine_spec.rb b/spec/vending_machine_spec.rb
    index 86f7465..e4a7602 100644
    --- a/spec/vending_machine_spec.rb
    +++ b/spec/vending_machine_spec.rb
    @@ -16,5 +16,61 @@ describe VendingMachine do
           vending_machine.drop_in(money)
           vending_machine.amount_of_drop_in.should == money
         end
    +    it "50円" do
    +      money = 50
    +      vending_machine.drop_in(money)
    +      vending_machine.amount_of_drop_in.should == money
    +    end
    +    it "100円" do
    +      money = 100
    +      vending_machine.drop_in(money)
    +      vending_machine.amount_of_drop_in.should == money
    +    end
    +    it "500円" do
    +      money = 500
    +      vending_machine.drop_in(money)
    +      vending_machine.amount_of_drop_in.should == money
    +    end
    +    it "1000円" do
    +      money = 1000
    +      vending_machine.drop_in(money)
    +      vending_machine.amount_of_drop_in.should == money
    +    end
       end
    +
    +  describe "許可しないお金を入れた場合" do
    +    it "1円" do
    +      proc { vending_machine.drop_in(1)
    +      }.should raise_error
    +    end
    +    it "5円" do
    +      proc { vending_machine.drop_in(5)
    +      }.should raise_error
    +    end
    +  end
    +
    +  describe "2つ以上の有効なお金を入れた場合" do
    +    describe "有効なお金だけの場合" do
    +      it "10円,50円,10円" do
    +        vending_machine.drop_in(10, 50, 10)
    +        vending_machine.amount_of_drop_in.should == 70
    +      end
    +      it "10円,50円,100円" do
    +        vending_machine.drop_in(10, 50, 100)
    +        vending_machine.amount_of_drop_in.should == 160
    +      end
    +    end
    +    describe "1つ以上の無効なお金が含まれる場合" do
    +      it "10円,1円,100円" do
    +        proc {
    +          vending_machine.drop_in(10,1,100)
    +        }.should raise_error
    +      end
    +      it "1円,100円" do
    +        proc {
    +          vending_machine.drop_in(1,100)
    +        }.should raise_error
    +      end
    +    end
    +   end
     end

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -8,8 +8,13 @@ class VendingMachine
     @amount
   end
 
-  def drop_in(money)
-    @amount = money
+  def drop_in(*money)
+    @amount = money.inject(0) do |sum, item|
+      if item == 1 || item == 5
+        raise "invalid money"
+      end
+      sum += item
+    end
   end
 end
 
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -16,5 +16,61 @@ describe VendingMachine do
       vending_machine.drop_in(money)
       vending_machine.amount_of_drop_in.should == money
     end
+    it "50円" do
+      money = 50
+      vending_machine.drop_in(money)
+      vending_machine.amount_of_drop_in.should == money
+    end
+    it "100円" do
+      money = 100
+      vending_machine.drop_in(money)
+      vending_machine.amount_of_drop_in.should == money
+    end
+    it "500円" do
+      money = 500
+      vending_machine.drop_in(money)
+      vending_machine.amount_of_drop_in.should == money
+    end
+    it "1000円" do
+      money = 1000
+      vending_machine.drop_in(money)
+      vending_machine.amount_of_drop_in.should == money
+    end
   end
+
+  describe "許可しないお金を入れた場合" do
+    it "1円" do
+      proc { vending_machine.drop_in(1)
+      }.should raise_error
+    end
+    it "5円" do
+      proc { vending_machine.drop_in(5)
+      }.should raise_error
+    end
+  end
+
+  describe "2つ以上の有効なお金を入れた場合" do
+    describe "有効なお金だけの場合" do
+      it "10円,50円,10円" do
+        vending_machine.drop_in(10, 50, 10)
+        vending_machine.amount_of_drop_in.should == 70
+      end
+      it "10円,50円,100円" do
+        vending_machine.drop_in(10, 50, 100)
+        vending_machine.amount_of_drop_in.should == 160
+      end
+    end
+    describe "1つ以上の無効なお金が含まれる場合" do
+      it "10円,1円,100円" do
+        proc {
+          vending_machine.drop_in(10,1,100)
+        }.should raise_error
+      end
+      it "1円,100円" do
+        proc {
+          vending_machine.drop_in(1,100)
+        }.should raise_error
+      end
+    end
+   end
 end
